bec06f201ee71b0c3c30f77369dad7603d9a4694
AdvayRChandra <advay.chandra@protonmail.com>
2025-11-03 14:59:08 -0500

    Implement initial System class with constructor and placeholder for mass conversion

---
+++ systems/processes.py
@@ -0,0 +1,12 @@
+
+
+class System:
+    def __init__(self, name, inputs=dict(), outputs=dict(), process=None):
+        self.name = name
+        self.inputs = inputs
+        self.outputs = outputs
+        self.process = process
+    
+    def convertMass():
+        pass
+        # Placeholder for mass conversion logic
