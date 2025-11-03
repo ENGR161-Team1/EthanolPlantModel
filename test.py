7573b86b4a8fe95bd3f0a91362553672a6409d38
AdvayRChandra <advay.chandra@protonmail.com>
2025-11-03 16:34:59 -0500

    Add test cases for Filtration class in test.py

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-from systems.processes import Fermentation
+from systems.processes import Fermentation, Filtration
 
+# Test Fermentation and Filtration classes
 test_fermenter = Fermentation(0.85)
-
 print(test_fermenter.name)
-
 print(test_fermenter.ferment({
     "ethanol": 0,
     "water": 100,
     "sugar": 50,
+    "fiber": 20
+}))
+
+test_filter = Filtration(0.90)
+print(test_filter.name)
+print(test_filter.filter({
+    "ethanol": 25.5,
+    "water": 100,
+    "sugar": 7.5,
     "fiber": 20
 }))
