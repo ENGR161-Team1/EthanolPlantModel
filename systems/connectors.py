ad1b56be4fbc834771096774c8ad8d42acbefcbc
AdvayRChandra <advay.chandra@protonmail.com>
2025-11-07 10:57:01 -0500

    Refactor Connector class to use kwargs for initialization and update Pipe class methods for energy and mass calculations

--- systems/connectors.py
+++ systems/connectors.py
@@ -2,29 +2,41 @@ import numpy as np
 import math
 
 class Connector:
-    def __init__(self, inputFlow, outputFlow, connectorFunction):
-        self.inputFlow = inputFlow
-        self.outputFlow = outputFlow
-        self.connectorFunction = connectorFunction
+    def __init__(self, **kwargs):
+        self.mass_function = kwargs.get("mass_function", None)
+        self.energy_function = kwargs.get("energy_function", None)
     
-    def pass_flow(self, **kwargs):
-        input_composition = kwargs.get("input_composition", dict())
-        total_flow = kwargs.get("total_flow", None)
-        return self.connectorFunction(input)
+    def processDensity(self, inputs=dict()):
+        input_flow = inputs.get("input_flow", 0) # input volumetric flow rate in m3/s
+        input_mass = inputs.get("input_mass", 0) # input mass flow rate in kg
+        return input_mass / input_flow if input_flow != 0 else 0
 
 class Pipe(Connector):
-    def __init__(self):
-        super().__init__("Pipe Input Flow", "Pipe Output Flow", self.pipe_function)
+    def __init__(self, **kwargs):
+        self.diameter = kwargs.get("diameter", 0.1)  # default diameter in meters
+        self.length = kwargs.get("length", 1.0)      # default length in meters
+        self.friction_factor = kwargs.get("friction_factor", 0.02)  # default friction factor
+
+        super().__init__(mass_function=self.pipeMassFunction, energy_function=self.pipeEnergyFunction)
         # Additional initialization for Pipe can go here
-
     
-    def pipe_function(self, input=dict()):
-        return input
-        # pass
+    def pipeEnergyFunction(self, **kwargs):
+        input_flow = kwargs.get("input_flow", 0)
+        input_mass = kwargs.get("input_mass", 0)
+        input_energy = kwargs.get("input_energy", 0)
+        interval = kwargs.get("interval", 1.0)
+        density = self.processDensity({"input_flow": input_flow, "input_mass": input_mass})
+        # For simplicity, we assume no change in composition, mass, or energy in the pipe
+        energy_change = density * interval * (8 * self.friction_factor * input_flow**3) / (math.pi**2 * self.diameter**5)
+        return input_energy - energy_change
+    
+    def pipeMassFunction(self, **kwargs):
+        pass
 
+"""
 class Bend(Connector):
     def __init__(self):
-        super().__init__("Bend Input Flow", "Bend Output Flow", self.bend_function)
+        super().__init__(connectorFunction=self.bend_function)
         # Additional initialization for Bend can go here
 
     
@@ -34,10 +46,11 @@ class Bend(Connector):
 
 class Valve(Connector):
     def __init__(self):
-        super().__init__("Valve Input Flow", "Valve Output Flow", self.valve_function)
+        super().__init__(connectorFunction=self.valve_function)
         # Additional initialization for Valve can go here
 
     
     def valve_function(self, input=dict()):
         return input
         # pass
+"""
