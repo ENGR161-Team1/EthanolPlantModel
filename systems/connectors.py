cd9449903acfbe3f8dfe804729e0dac583100e31
AdvayRChandra <advay.chandra@protonmail.com>
2025-11-07 11:20:14 -0500

    Implement Bend class with energy and mass functions, enhancing flow calculations

--- systems/connectors.py
+++ systems/connectors.py
@@ -33,17 +33,32 @@ class Pipe(Connector):
     def pipeMassFunction(self, **kwargs):
         pass
 
-"""
 class Bend(Connector):
-    def __init__(self):
-        super().__init__(connectorFunction=self.bend_function)
+    def __init__(self, **kwargs):
+        self.bend_radius = kwargs.get("bend_radius", 0.5)  # default bend radius in meters
+        bend_factor = kwargs.get("bend_factor", 0.9)  # default bend factor
         # Additional initialization for Bend can go here
+        super().__init__(mass_function=self.bendMassFunction, energy_function=self.bendEnergyFunction)
 
     
-    def bend_function(self, input=dict()):
-        return input
-        # pass
+    def bendEnergyFunction(self, **kwargs):
+        input_flow = kwargs.get("input_flow", 0)
+        input_mass = kwargs.get("input_mass", 0)
+        input_energy = kwargs.get("input_energy", 0)
+        interval = kwargs.get("interval", 1.0)
+        density = self.processDensity({"input_flow": input_flow, "input_mass": input_mass})
+        # For simplicity, we assume no change in composition, mass, or energy in the bend
 
+        velocity = input_flow / ((math.pi / 4) * (self.bend_radius ** 2))
+        energy_change = density * interval * (1 - self.bend_factor) * (velocity ** 2) / 2
+        return input_energy - energy_change
+    
+    def bendMassFunction(self, **kwargs):
+        pass
+
+    
+
+"""
 class Valve(Connector):
     def __init__(self):
         super().__init__(connectorFunction=self.valve_function)
